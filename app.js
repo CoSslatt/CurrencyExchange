f49d6d97623fb902daae3bf03320592e37d7fde4
Narcikss <mdgamedeveloper@gmail.com>
2022-05-29 22:59:17 +0200

    change normal fetch to async and await

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const API_LINK = "http://api.nbp.pl/api/exchangerates/tables/";
+const PROPERLY_STATUS = 200;
 const TABLES = ["A", "B"];
 
-function searchForCurrency() {
+const searchForCurrency = async () => {
 	for (let i = 0; i < TABLES.length; i++) {
-		fetch(API_LINK + TABLES[i], {
-			method: "GET",
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				for (let i = 0; i < data[0].rates.length; i++) {
-					if (data[0].rates[i].code === "RUB") {
-						console.log(data[0].rates[i], "table: " + data[0].table);
+		const response = await fetch(API_LINK + TABLES[i]);
 
-						break;
-					} else continue;
-				}
-			});
+		if (response.status !== PROPERLY_STATUS)
+			throw new Error("The file doesn't exist");
+
+		return await response.json();
 	}
-}
+};
 
-searchForCurrency();
+searchForCurrency()
+	.then((data) => {
+		for (let i = 0; i < data[0].rates.length; i++) {
+			if (data[0].rates[i].code === "USD") {
+				console.log(data[0].rates[i], "table: " + data[0].table);
+
+				break;
+			} else continue;
+		}
+	})
+	.catch((err) => {
+		console.log("Error: ", err.message);
+	});
