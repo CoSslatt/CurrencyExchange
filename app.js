b744929f72cee4810090434ee57d1d8ee3b85cb8
Narcikss <mdgamedeveloper@gmail.com>
2022-05-30 21:47:53 +0200

    add searching currencies at console idk how to write that

--- app.js
+++ app.js
@@ -13,22 +13,21 @@ const availableCurrencies = [
 const TABLES = ["A", "B"];
 
 const submit = document.querySelector(".submit");
+const selectForCurrencies = document.querySelector(".select__for__currencies");
 const firstCurrencyInput = document.querySelector(".from__currency__input");
 const secondCurrencyInput = document.querySelector(".to__currency__input");
 
-submit.addEventListener("submit", (e) => {
-	e.preventDefault();
+let currentCurrency;
 
-	searchForCurrency()
-		.then((data) => {
-			for (let i = 0; i < availableCurrencies.length; i++) {}
-		})
-		.catch((err) => {
-			console.log("Error: ", err.message);
-		});
+submit.addEventListener("click", (e) => {
+	exchangeValues();
 });
 
-function exchangeValues() {}
+function exchangeValues() {
+	searchForCurrency().catch((err) => {
+		console.log("Error: ", err.message);
+	});
+}
 
 const searchForCurrency = async () => {
 	for (let i = 0; i < TABLES.length; i++) {
@@ -37,6 +36,15 @@ const searchForCurrency = async () => {
 		if (response.status !== PROPERLY_STATUS)
 			throw new Error("The file doesn't exist");
 
-		return await response.json();
+		const data = await response.json();
+
+		for (let i = 0; i < data[0].rates.length; i++) {
+			if (selectForCurrencies.value == data[0].rates[i].currency) {
+				currentCurrency = data[0].rates[i];
+				console.log(currentCurrency);
+
+				break;
+			}
+		}
 	}
 };
